a451e3000fc2b94340a265c2acbc315ed079414f
caue <cauepaivalira@outlook.com>
2024-10-27 21:02:23 -0300

    Change1: Adicionei uma função de busca em grafos com pesos genérica com heap e agora o Dijsktra e o A* usam ela, com o A* passando um funcao de euristica

--- GraphSearch.py
+++ GraphSearch.py
@@ -1,6 +1,8 @@
 from KnnGraph import KnnGraph
 import heapq
 from dataclasses import dataclass
+from typing import Callable
+
 
 class GraphSearch:
 
@@ -68,57 +70,10 @@ class GraphSearch:
                   stack.append(vertex_index)
 
       return [] #não achou o caminho
+   
+   def __weighted_graph_search(self,start_vertex:int,end_vertex:int, heuristic_fn:Callable[[int,int],float]| None = None)->list[int]:
+      search_heuristic_fn: Callable[[int, int], float] = heuristic_fn if heuristic_fn is not None else lambda x, y: 0
 
-   def dijkstra(self,start_vertex:int,end_vertex:int)->list[int]:
-      INF = float('inf')
-      @dataclass
-      class VertexInfo:
-         parent:int = -1
-         weight:float = INF
-         explored:bool = False
-
-      vertex_list:list[VertexInfo] = [VertexInfo() for x in range(self.__n)] #
-      vertex_list[start_vertex].weight = 0 #atualiza o peso do primeiro cara
-      path_found: bool = False
-      adj_matrix:list[list[float]] = self.__graph.get_adj_matrix()
-
-      heap:list[tuple[float,int]] = []
-      heapq.heappush(heap,(0,start_vertex))
-
-      while heap:
-         cur_weight,cur_index, = heapq.heappop(heap) #pop vertice com menor peso
-         cur_vertex:VertexInfo = vertex_list[cur_index] #acha o objeto vertex associado
-
-         if cur_vertex.explored: #ja foi explorado
-            continue
-         
-         cur_vertex.explored = True #marca como explorado
-         if cur_index == end_vertex: #vertice final
-            path_found = True
-            break
-         #relaxamento
-         for vertex_index,weight in enumerate(adj_matrix[cur_index]): #loop pelos vizinhos
-            if weight != self.__graph.NO_EDGE and not vertex_list[vertex_index].explored: #aresta existe e não visitamos o vértice ainda
-               new_dist = cur_vertex.weight + adj_matrix[cur_index][vertex_index] #distancia do nó pai + aresta
-               old_dist = vertex_list[vertex_index].weight #distancia antiga
-
-               if new_dist < old_dist: #nova distancia é menor que a anterior
-                  vertex_list[vertex_index].parent = cur_index #atualiza parente
-                  vertex_list[vertex_index].weight = new_dist #atualiza distancia
-                  heapq.heappush(heap,(new_dist,vertex_index)) #push na heap de uma tupla com o novo peso e o index do vertice antigo
-
-      if path_found:
-         path = []
-         cur = end_vertex
-         while cur != -1:
-            path.append(cur)
-            cur = vertex_list[cur].parent
-         path.reverse()
-         return path
-      else:
-         return []
-      
-   def a_star(self,start_vertex:int,end_vertex:int)->list[int]:
       INF = float('inf')
       @dataclass
       class VertexInfo:
@@ -155,7 +110,7 @@ class GraphSearch:
                   vertex_list[vertex_index].parent = cur_index #atualiza parente
                   vertex_list[vertex_index].weight = new_dist #atualiza distancia
 
-                  heuristic_weight:float = new_dist + self.__graph.euclidian_dist_between(vertex_index,end_vertex) #A* utiliza a heuristica de somar a distancia euclidiana entre o nó atual e o nó origem
+                  heuristic_weight:float = new_dist + search_heuristic_fn(vertex_index,end_vertex)
                   heapq.heappush(heap,(heuristic_weight,vertex_index)) #push na heap de uma tupla com o novo peso e o index do vertice antigo
 
       if path_found:
@@ -168,10 +123,17 @@ class GraphSearch:
          return path
       else:
          return []
+         
+
+   def dijkstra(self,start_vertex:int,end_vertex:int)->list[int]:
+      return self.__weighted_graph_search(start_vertex,end_vertex)
+      
+   def a_star(self,start_vertex:int,end_vertex:int)->list[int]:
+      return self.__weighted_graph_search(start_vertex,end_vertex,self.__graph.euclidian_dist_between)
 
    def plot_path(self,path:list[int])->None:
       if not path:
-         print("Caminho vazio, não é possível desenhar")
+         print("Caminho não existe, não é possível desenhar")
          return
       import networkx as nx
       import matplotlib.pyplot as plt
@@ -208,5 +170,5 @@ if __name__ == "__main__":
    graph = KnnGraph(10,2)
    search = GraphSearch(graph)
 
-   bfs_walk = search.a_star(0,7)
+   bfs_walk = search.dijkstra(0,7)
    search.plot_path(bfs_walk)
